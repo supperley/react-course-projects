930bb30769f7d5aec0e128e944506a7f4b5722de
supperley <v.bobrovko@gmail.com>
2023-03-23 10:56:54 +0300

    Dynamic year filter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 // import React from "react";
+import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
+import { useState } from "react";
+
+const INITIAL_COSTS = [
+    {
+        id: "c1",
+        date: new Date(2021, 2, 12),
+        description: "Холодильник",
+        amount: 999.99,
+    },
+    {
+        id: "c2",
+        date: new Date(2021, 11, 25),
+        description: "MacBook",
+        amount: 1254.72,
+    },
+    {
+        id: "c3",
+        date: new Date(2021, 3, 1),
+        description: "Джинсы",
+        amount: 49.99,
+    },
+];
 
 const App = () => {
-  const costs = [
-    {
-      date: new Date(2021, 2, 12),
-      description: "Холодильник",
-      amount: 999.99,
-    },
-    {
-      date: new Date(2021, 11, 25),
-      description: "MacBook",
-      amount: 1254.72,
-    },
-    {
-      date: new Date(2021, 3, 1),
-      description: "Джинсы",
-      amount: 49.99,
-    },
-  ];
+    const [costs, setCosts] = useState(INITIAL_COSTS);
 
-  // return React.createElement(
-  //   "div",
-  //   {},
-  //   React.createElement("h1", {}, "Начнём изучение React!"),
-  //   React.createElement(Costs, { costs: costs })
-  // );
+    // return React.createElement(
+    //   "div",
+    //   {},
+    //   React.createElement("h1", {}, "Начнём изучение React!"),
+    //   React.createElement(Costs, { costs: costs })
+    // );
 
-  return (
-    <div>
-      <h1>Начнём изучение React!</h1>
-      <Costs costs={costs} />
-    </div>
-  );
+    const addCostHandler = (cost) => {
+        setCosts((prevCosts) => [cost, ...costs]);
+    };
+
+    return (
+        <div>
+            <NewCost onAddCost={addCostHandler} />
+            <Costs costs={costs} />
+        </div>
+    );
 };
 
 export default App;
--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,16 +1,17 @@
-import React from 'react';
 import "./CostItem.css";
+import CostDate from "./CostDate";
+import Card from "../UI/Card";
 
-const CostItem = () => {
-    return (
-        <div className='cost-item'>
-            <div>март 2021 12</div>
-            <div className='cost-item__description'>
-                <h2>Холодильник</h2>
-                <div className='cost-item__price'>$999.99</div>
-            </div>
-        </div>
-    );
+const CostItem = (props) => {
+  return (
+    <Card className="cost-item">
+      <CostDate date={props.date} />
+      <div className="cost-item__description">
+        <h2>{props.description}</h2>
+        <div className="cost-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
 };
 
 export default CostItem;
