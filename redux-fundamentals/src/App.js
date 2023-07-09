5c9e4ed58e6f8a92f44b7667139524b60c650402
supperley <v.bobrovko@gmail.com>
2023-07-09 11:35:52 +0300

    add redux

--- redux-fundamentals/src/App.js
+++ redux-fundamentals/src/App.js
@@ -1,11 +1,11 @@
-import Counter from "./components/Counter";
+import Counter from './components/Counter';
 
 function App() {
-  return (
-    <div>
-      <Counter />
-    </div>
-  );
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
 }
 
 export default App;
--- redux-fundamentals/src/components/Counter.js
+++ redux-fundamentals/src/components/Counter.js
@@ -1,15 +1,19 @@
-import classes from "./Counter.module.css";
+import { useSelector } from 'react-redux';
+
+import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+    const counter = useSelector((state) => state.counter);
 
-  return (
-    <main className={classes.counter}>
-      <h1>Счётчик</h1>
-      <div className={classes.value}>0</div>
-      <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
-    </main>
-  );
+    const toggleCounterHandler = () => {};
+
+    return (
+        <main className={classes.counter}>
+            <h1>Счётчик</h1>
+            <div className={classes.value}>{counter}</div>
+            <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
+        </main>
+    );
 };
 
 export default Counter;
