eccfe2452c50e1388ff595e5bd57554b72c8a7cf
supperley <v.bobrovko@gmail.com>
2023-07-09 12:36:45 +0300

    add increase action

--- redux-fundamentals/src/components/Counter.js
+++ redux-fundamentals/src/components/Counter.js
@@ -1,19 +1,85 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+// import { Component } from "react";
+// import { connect } from "react-redux";
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+    const dispatchFunction = useDispatch();
     const counter = useSelector((state) => state.counter);
+    const isCounterInvisible = useSelector((state) => state.isCounterInvisible);
 
-    const toggleCounterHandler = () => {};
+    const incrementHandler = () => {
+        dispatchFunction({ type: 'increment' });
+    };
+
+    const decrementHandler = () => {
+        dispatchFunction({ type: 'decrement' });
+    };
+
+    const increaseHandler = () => {
+        dispatchFunction({ type: 'increase', number: 100 });
+    };
+
+    const toggleCounterHandler = () => {
+        dispatchFunction({ type: 'visibility' });
+    };
 
     return (
         <main className={classes.counter}>
             <h1>Счётчик</h1>
-            <div className={classes.value}>{counter}</div>
+            {!isCounterInvisible && (
+                <div className={classes.value}>{counter}</div>
+            )}
+            <div>
+                <button onClick={incrementHandler}>+</button>
+                <button onClick={increaseHandler}>+100</button>
+                <button onClick={decrementHandler}>-</button>
+            </div>
             <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
         </main>
     );
 };
 
 export default Counter;
+
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Счётчик</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>+</button>
+//           <button onClick={this.decrementHandler.bind(this)}>-</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Спрятать / Показать</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- redux-fundamentals/src/store/index.js
+++ redux-fundamentals/src/store/index.js
@@ -1,16 +1,37 @@
 import { legacy_createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, isCounterInvisible: false };
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
         return {
             counter: state.counter + 1,
+            isCounterInvisible: state.isCounterInvisible,
         };
     }
+
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.number,
+            isCounterInvisible: state.isCounterInvisible,
+        };
+    }
+
     if (action.type === 'decrement') {
         return {
             counter: state.counter - 1,
+            isCounterInvisible: state.isCounterInvisible,
         };
     }
+
+    if (action.type === 'visibility') {
+        return {
+            counter: state.counter,
+            isCounterInvisible: !state.isCounterInvisible,
+        };
+    }
+
+    return state;
 };
 
 const store = legacy_createStore(counterReducer);
