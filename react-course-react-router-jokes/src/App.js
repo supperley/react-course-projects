ef9b374dcc484b56f155b09fe7b361849769ce5d
supperley <v.bobrovko@gmail.com>
2023-07-22 16:57:55 +0300

    add app pages

--- react-course-react-router-jokes/src/App.js
+++ react-course-react-router-jokes/src/App.js
@@ -1,7 +1,32 @@
-import Header from './components/layout/Header';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Jokes from './pages/Jokes';
+import JokeDetails from './pages/JokeDetails';
+import AddJoke from './pages/AddJoke';
+import Layout from './components/layout/Layout';
+import NotFound from './pages/NotFound';
 
 function App() {
-    return <div></div>;
+    return (
+        <Layout>
+            <Switch>
+                <Route path="/" exact>
+                    <Redirect to="/jokes" />
+                </Route>
+                <Route path="/jokes" exact>
+                    <Jokes />
+                </Route>
+                <Route path="/jokes/:jokeId">
+                    <JokeDetails />
+                </Route>
+                <Route path="/add-joke">
+                    <AddJoke />
+                </Route>
+                <Route path="*">
+                    <NotFound />
+                </Route>
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
---
+++ react-course-react-router-jokes/src/pages/JokeDetails.js
@@ -0,0 +1,38 @@
+import { useParams, Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
+import HighlightedJoke from '../components/jokes/HighlightedJoke';
+
+const DUMMY_JOKES = [
+    {
+        id: 'j1',
+        topic: 'Programming',
+        text: `How many programmers does it take to change a light bulb?
+    None – It’s a hardware problem`,
+    },
+    {
+        id: 'j2',
+        topic: 'General',
+        text: `How many bones are in the human hand?
+    A handful of them.`,
+    },
+];
+
+const JokeDetails = () => {
+    const params = useParams();
+    const joke = DUMMY_JOKES.find((joke) => joke.id === params.jokeId);
+
+    if (!joke) {
+        return <h1 className="centered">Шуток не найдено</h1>;
+    }
+
+    return (
+        <>
+            <HighlightedJoke text={joke.text} topic={joke.topic} />
+            <Route path="/jokes/:jokeId/comments">
+                <Comments />
+            </Route>
+        </>
+    );
+};
+
+export default JokeDetails;
